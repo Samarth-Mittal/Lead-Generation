0e9be3cdb566bab099cbba13f621b0944d575625
Samarth Mittal <samarth.mittal@practo.com>
2023-08-30 19:18:39 +0530

    Initial commit

---
+++ src/main/java/com/example/Lead/Generation/LeadGenerationConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lead.Generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadGenerationConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadGenerationConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lead/Generation/LeadGenerationConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lead.Generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadGenerationConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
